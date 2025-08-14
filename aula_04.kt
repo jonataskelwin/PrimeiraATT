ca884d9c30af747d3de3f473d0d0334e8cfa82b8
jonataskelwin <jonataskelwin048@gmail.com>
2025-08-13 21:35:50 -0300

    Create aula_04.kt

---
+++ aula_04.kt
@@ -0,0 +1,15 @@
+fun main () {
+    var clientes = 11 
+    
+    clientes += 7 
+    println(clientes)
+    
+    clientes  -= 3
+    println(clientes)
+
+    clientes  *= 2
+    println(clientes)
+
+    clientes  /= 3
+    println(clientes)
+}
